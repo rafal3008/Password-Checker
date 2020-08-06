25d54a82168abcdba7f3be02c0e90c6eb07b0bd0
Rafał Pyżalski <rafal.pyzalski08@gmail.com>
2020-08-07 00:18:02 +0200

    update check_if_pwned_password

--- checker.py
+++ checker.py
@@ -4,16 +4,20 @@ import hashlib
 
 def request_api_data(query_char):
     # request data from server
-    url = 'https://api.pwnedpasswords.com/range/' + query_char  # need to be a hash
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
     response = requests.get(url)
     return response
 
 
 def check_if_pwned_password(password):
-    # need to encode password, and then hash it
     hash_password = (hashlib.sha1(password.encode('utf-8')).hexdigest())
     hash_password = hash_password.upper()
-    print(hash_password)
+    first_five = hash_password[:5]
+    tail = hash_password[5:]
+    print(first_five, tail)
+    response = request_api_data(first_five)
+
+    print(response)
 
 
 check_if_pwned_password('password')
