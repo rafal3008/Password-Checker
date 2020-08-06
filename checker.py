cb3865da82ad86559a4d580541776e3d9d1c52ab
Rafał Pyżalski <rafal.pyzalski08@gmail.com>
2020-08-07 00:12:13 +0200

    added check_if_pwned_password func
    added check_if_pwned_password function, to hash password created by user

--- checker.py
+++ checker.py
@@ -1,12 +1,19 @@
 import requests
+import hashlib
 
 
 def request_api_data(query_char):
     # request data from server
     url = 'https://api.pwnedpasswords.com/range/' + query_char  # need to be a hash
     response = requests.get(url)
-    print(response)  # if everything is good, should get <Response [200]>
     return response
 
 
-request_api_data('CBDFA')
+def check_if_pwned_password(password):
+    # need to encode password, and then hash it
+    hash_password = (hashlib.sha1(password.encode('utf-8')).hexdigest())
+    hash_password = hash_password.upper()
+    print(hash_password)
+
+
+check_if_pwned_password('password')
